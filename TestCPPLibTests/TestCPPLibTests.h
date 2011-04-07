bf6fd23e174048f13fe6bc92cd7cc6b6e5b6191b
KwangSeob Jeong <gsjung@ktnet.com>
2011-04-07 14:27:56 +0900

    Initial Commit

---
+++ TestCPPLibTests/TestCPPLibTests.h
@@ -0,0 +1,17 @@
+//
+//  TestCPPLibTests.h
+//  TestCPPLibTests
+//
+//  Created by lesstif on 11. 4. 7..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TestCPPLibTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ TestCPPLibTests/TestCPPLibTests.m
@@ -0,0 +1,33 @@
+//
+//  TestCPPLibTests.m
+//  TestCPPLibTests
+//
+//  Created by lesstif on 11. 4. 7..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestCPPLibTests.h"
+
+
+@implementation TestCPPLibTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestCPPLibTests");
+}
+
+@end
